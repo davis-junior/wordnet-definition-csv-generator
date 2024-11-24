a7122ba5d010b41d5b0e858185dbb008955e7d08
David Kornacki <davis-junior@users.noreply.github.com>
2024-11-24 15:29:47 -0500

    Remove WordNet comments

--- wordnet_definition_csv_generator.py
+++ wordnet_definition_csv_generator.py
@@ -43,7 +43,20 @@ def get_output_filename(input_file: str) -> str:
 
 def clean_definition(definition: str) -> str:
     definition = definition.replace(":", " -- ")
-    definition = definition.replace("; ; ; ; - Karen Horney", "")
+    
+    # remove WordNet comments
+    comment_index = definition.find("; ;")
+    if comment_index >= 0:
+        definition = definition[:comment_index]
+
+    comment_index = definition.find("; -")
+    if comment_index >= 0:
+        definition = definition[:comment_index]
+
+    while definition.endswith(";") or definition.endswith(" "):
+        definition = definition.rstrip(";")
+        definition = definition.rstrip(" ")
+
     definition = definition.replace('"', "'")
     definition = definition.replace("    ", " ")
     definition = definition.replace("   ", " ")
